b928f4370f301d66c8171a6a2d71385efe1e2aab
Darshil Shah <darshilx@amazon.com>
2022-12-18 19:13:43 +0530

    Slider-3 : Add the first slide

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,7 +6,6 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
   position: relative;
 `
 
@@ -30,12 +29,67 @@ const Arrow = styled.div`
   opacity: 0.5;
 `
 
+const Wrapper = styled.div`
+  height: 100%;
+`
+
+const Slide = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`
+
+const ImgContainer = styled.div`
+  flex:1;
+  height: 100%;
+`
+
+const Image = styled.img`
+  height: 80%;
+`
+
+const InfoContainer = styled.div`
+  flex:1;
+  padding: 50px;
+`
+
+const Title = styled.h1`
+  font-size: 60px;
+`
+
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: bold;
+  letter-spacing: 3px;
+`
+
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`
+
 const Slider = () => {
   return (
     <Container>
       <Arrow direction="left">
         <ArrowLeftOutlined/>
       </Arrow>
+      <Wrapper>
+        <Slide>
+          <ImgContainer>
+            <Image src="https://images.unsplash.com/photo-1568251188392-ae32f898cb3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2362&q=80"/>
+          </ImgContainer>
+          <InfoContainer>
+            <Title>SUMMER SALE</Title>
+            <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+            <Button>SHOP NOW</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
       <Arrow direction="right">
         <ArrowRightOutlined/>
       </Arrow>
