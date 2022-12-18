8345b4bb36af88487e3f37eaf4de15b0708ce9de
Darshil Shah <darshilx@amazon.com>
2022-12-18 16:23:00 +0530

    Navbar-1 : Add basic layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,10 +10,32 @@ const Container = styled.div`
   height: 60px;
 `
 
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`
+
+const Left = styled.div`
+  flex: 1; // Allocate equal space to all three divs -> left, center, right
+`
+
+const Center = styled.div`
+  flex: 1; // Allocate equal space to all three divs -> left, center, right
+`
+
+const Right = styled.div`
+  flex: 1; // Allocate equal space to all three divs -> left, center, right
+`
+
 const Navbar = () => {
   return (
     <Container>
-      Navbar
+      <Wrapper>
+        <Left>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, dicta ducimus commodi quod dignissimos obcaecati dolore ipsum minima asperiores exercitationem molestiae corporis ex, omnis fugiat aspernatur fuga distinctio, sint nemo!</Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
     </Container>
   )
 }
