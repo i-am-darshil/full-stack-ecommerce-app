5ed675e7316f440e86ff9768ab62e550a4ccc2b3
Darshil Shah <darshilx@amazon.com>
2022-12-18 16:47:25 +0530

    Navbar-4 : Add right side

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { Search } from '@material-ui/icons'
+import { Badge } from '@material-ui/core'
+import { Search, ShoppingCartOutlined } from '@material-ui/icons'
 import React from 'react'
 import styled from 'styled-components'
 
@@ -55,6 +56,15 @@ const Logo = styled.div`
 
 const Right = styled.div`
   flex: 1; // Allocate equal space to all three divs -> left, center, right
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;;
 `
 
 const Navbar = () => {
@@ -65,13 +75,21 @@ const Navbar = () => {
           <Language>EN</Language>
           <SearchContainer>
             <Input />
-            <Search />
+            <Search style={{color:"gray", fontSize:16}}/>
           </SearchContainer>
         </Left>
         <Center>
           <Logo>LAMA.</Logo>
         </Center>
-        <Right>Right</Right>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined/>
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   )
