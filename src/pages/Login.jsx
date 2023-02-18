34dc4bd6bae4f4d4d456db1f647bbc2e53ddfe2b
Darshil Shah <darshilx@amazon.com>
2023-02-19 00:21:30 +0530

    Add Login and Register Page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,109 @@
+import styled from "styled-components";
+import RegisterImage from '../images/register.jpg';
+
+
+const LeftContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        rgba(255, 255, 255, 0.2),
+        rgba(255, 255, 255, 0.2)
+      ),
+      url(${(props) => props.img})
+        right;
+  background-size: cover;
+  flex: 1.4;
+
+`
+
+const RightContainer = styled.div`
+  width: 100vw;
+    height: 100vh;
+    background: white;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex:1;
+`
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 80%;
+  padding: 20px;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+  text-align: center;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  margin: auto;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+
+const Link = styled.a`
+  /* margin: 5px 0px; */
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+  margin: auto;
+  margin-bottom: 10px;
+
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <LeftContainer img={RegisterImage}>
+
+      </LeftContainer>
+
+      <RightContainer>
+        <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="username" />
+          <Input placeholder="password" />
+          <Button>LOGIN</Button>
+          <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
+          <Link>CREATE A NEW ACCOUNT</Link>
+        </Form>
+        </Wrapper>
+      </RightContainer>
+      
+    </Container>
+  );
+};
+
+export default Login;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,108 @@
+import styled from "styled-components";
+import RegisterImage from '../images/register.jpg';
+
+
+const LeftContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        rgba(255, 255, 255, 0.2),
+        rgba(255, 255, 255, 0.2)
+      ),
+      url(${(props) => props.img})
+        right;
+  background-size: cover;
+  flex: 1.4;
+
+`
+
+const RightContainer = styled.div`
+  width: 100vw;
+    height: 100vh;
+    background: white;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex:1;
+`
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 20px;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+  text-align: center;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
+
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  margin: auto;
+`;
+
+const Register = () => {
+  return (
+    <Container>
+      <LeftContainer img={RegisterImage}>
+
+      </LeftContainer>
+
+      <RightContainer>
+        <Wrapper>
+          <Title>CREATE AN ACCOUNT</Title>
+          <Form>
+            <Input placeholder="name" />
+            <Input placeholder="last name" />
+            <Input placeholder="username" />
+            <Input placeholder="email" />
+            <Input placeholder="password" />
+            <Input placeholder="confirm password" />
+            <Agreement>
+              By creating an account, I consent to the processing of my personal
+              data in accordance with the <b>PRIVACY POLICY</b>
+            </Agreement>
+            <Button>CREATE</Button>
+          </Form>
+        </Wrapper>
+      </RightContainer>
+      
+    </Container>
+  );
+};
+
+export default Register;
